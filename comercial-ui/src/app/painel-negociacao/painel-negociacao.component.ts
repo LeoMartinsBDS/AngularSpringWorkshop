c77e320110ae45994398521773c57f568b9ef751
LeoMartinsBDS <34400625+LeoMartinsBDS@users.noreply.github.com>
2019-03-22 15:53:21 -0300

    UI

---
+++ comercial-ui/src/app/painel-negociacao/painel-negociacao.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { OportunidadeService } from '../oportunidade.service';
+import { MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-painel-negociacao',
+  templateUrl: './painel-negociacao.component.html',
+  styleUrls: ['./painel-negociacao.component.css']
+})
+export class PainelNegociacaoComponent implements OnInit {
+
+  oportunidade = {};
+  oportunidades = [];
+
+  constructor(private oportunidadeService: OportunidadeService,
+              private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.consultar();
+  }
+
+  adicionar(){
+    this.oportunidadeService.adicionar(this.oportunidade)
+      .subscribe(() => {
+        this.oportunidade = {};
+        this.consultar();
+        this.messageService.add({
+          severity: 'success',
+          summary: 'Oportunidade adicionado com sucesso!'
+        })
+      }, resposta => {
+        let msg = 'Erro inesperado. Tente novamente!'; 
+        if(resposta.error.message){
+          msg = resposta.error.message;
+        }
+        this.messageService.add({
+          severity: 'error',
+          summary: msg
+        })
+      });
+  }
+
+  consultar(){
+    this.oportunidadeService.listar()
+      .subscribe(resposta => this.oportunidades = <any>resposta);
+  }
+
+}
